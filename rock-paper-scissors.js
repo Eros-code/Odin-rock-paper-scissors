7120a3467be55555f3ccb7d495f74a625de2c8e3
Eros-code <eros.achillea@outlook.com>
2022-10-08 15:27:20 +0100

    Add playRound function allowing user input

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -6,6 +6,23 @@ function getComputerChoice() {
   return choiceArr[x];
 }
 
-computer = getComputerChoice();
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    console.log("It's a draw!");
+  } else if (
+    (playerSelection === "Rock" && computerSelection === "Paper") ||
+    (playerSelection === "Scissors" && computerSelection === "Rock") ||
+    (playerSelection === "Paper" && computerSelection === "Scissors")
+  ) {
+    console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
+  } else {
+    console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
+  }
+}
 
-console.log(computer);
+const computerSelection = getComputerChoice();
+const playerSelection = prompt("Enter Rock, Paper or Scissors!");
+
+playRound(playerSelection, computerSelection);
+
+//console.log(computerSelection);
