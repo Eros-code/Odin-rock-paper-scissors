c7a836e8976a8d98c810e73b000356b985467e4e
Eros-code <eros.achillea@outlook.com>
2022-10-09 13:21:18 +0100

    add game function to allow first to 5 adjustments made to other functions

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -8,24 +8,57 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
-    console.log("It's a draw!");
+    console.log("It's a draw!\n");
   } else if (
     (playerSelection === "Rock" && computerSelection === "Paper") ||
     (playerSelection === "Scissors" && computerSelection === "Rock") ||
     (playerSelection === "Paper" && computerSelection === "Scissors")
   ) {
-    console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
+    console.log(
+      `\nYou lose this round! ${computerSelection} beats ${playerSelection}\n`
+    );
+    return false;
   } else {
-    console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
+    console.log(
+      `\nYou win this round! ${playerSelection} beats ${computerSelection}\n`
+    );
+    return true;
   }
 }
 
-const computerSelection = getComputerChoice();
-let playerSelection = prompt("Enter Rock, Paper or Scissors!");
+function game() {
+  let playerScore = 0;
+  let computerScore = 0;
 
-playerSelection =
-  playerSelection[0].toUpperCase() + playerSelection.toLowerCase().slice(1);
+  while (playerScore < 5 && computerScore < 5) {
+    const computerSelection = getComputerChoice();
+    let playerSelection = prompt("Enter Rock, Paper or Scissors!");
 
-playRound(playerSelection, computerSelection);
+    playerSelection =
+      playerSelection[0].toUpperCase() + playerSelection.toLowerCase().slice(1);
+
+    let newRound = playRound(playerSelection, computerSelection);
+
+    if (newRound === true) {
+      playerScore += 1;
+    } else if (newRound === false) {
+      computerScore += 1;
+    }
+
+    console.log(
+      `You currently have a score of ${playerScore} and the computer has a score of ${computerScore}`
+    );
+  }
+
+  if (playerScore === 5) {
+    console.log("\nCongratulations you have won the game!");
+  } else if (computerScore === 5) {
+    console.log("\nYou lose! the computer has won the game!");
+  }
+
+  return null;
+}
+
+game();
 
 //console.log(computerSelection);
